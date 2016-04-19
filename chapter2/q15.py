e7d8e356b3f5e2354816e741226d7d9f7268c769
tanikawa04 <tanikawa04@gmail.com>
2016-04-20 00:22:38 +0900

    add q14, 15

---
+++ chapter2/q15.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import sys
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+
+    with open('hightemp.txt', 'r') as f:
+        lines = f.readlines()[-n:]
+
+    res = '\n'.join([line.rstrip() for line in lines])
+
+    print(res)
